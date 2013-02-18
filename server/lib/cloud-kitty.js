15e0d829224c4e2d58fa9853e23e1e19af6c3e63
Igor Zinoviev <igor@ktlabs.ru>
2013-02-18 22:37:49 +0400

    Some dummy codez

---
+++ server/lib/cloud-kitty.js
@@ -0,0 +1,11 @@
+/*
+ * cloud-kitty
+ * https://github.com/zinoviev/chrome-plugin-test
+ *
+ * Copyright (c) 2013 ZIgi
+ * Licensed under the MIT license.
+ */
+
+exports.awesome = function() {
+  return 'awesome';
+};
